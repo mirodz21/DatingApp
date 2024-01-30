534125b70f07a21a19655ad3e451dfc802f1fec0
ian rodriguez <mi.rodz21@gmail.com>
2024-01-30 14:24:50 +0800

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,15 +9,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnections"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass',
+})
+export class AppComponent implements OnInit {
+  title: string = 'DatingApp';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => (this.users = res),
+      error: (err) => console.log(err),
+      complete: () => console.log('Fetch Complete'),
+    });
+  }
+}
